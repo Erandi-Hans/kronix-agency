acccde9f23c4d9ae83945dc99c9bc0fa93f94c6e
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-03-21 04:59:09 +0530

    improve app.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,10 @@ import Process from './components/Process';
 
 function App() {
   return (
-    <div className="relative min-h-screen overflow-hidden text-white bg-black">
-      
-      <div className="absolute top-[-10%] left-1/2 -translate-x-1/2 w-[1000px] h-[600px] bg-[#bef264]/10 blur-[120px] rounded-full -z-10 pointer-events-none" />
-
-      <div 
-        className="absolute inset-0 opacity-[0.03] pointer-events-none -z-10" 
-        style={{ backgroundImage: `url('https://grainy-gradients.vercel.app/noise.svg')` }}
-      />
-
+    <div className="relative">
       <Navbar />
       <Hero />
       <Process />
-      
     </div>
   );
 }
