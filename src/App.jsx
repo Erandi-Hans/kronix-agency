27cdc8be048aec5ecdcf70ce75cbe3f110731403
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-04-10 11:39:50 +0530

    Solve error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react'; 
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
 
+import Hero from './components/Sections/Hero';
+import Process from './components/Sections/Process';
+import Portfolio from './components/Sections/Portfolio';
+import FAQ from './components/Sections/FAQ';
+import Benefits from './components/Sections/Benefits';
+import Services from './components/Sections/Services';
 
+import Pricing from './components/Sections/Pricing';
+import CTA from './components/Sections/Cta';
 
+import ContactModal from './components/Common/ContactModal';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
